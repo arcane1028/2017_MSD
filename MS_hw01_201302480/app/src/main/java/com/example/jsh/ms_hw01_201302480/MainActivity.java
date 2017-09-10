0532a3ef912b6105db490ec411c736eb3ddda528
arcane1028 <arcane1028@gmail.com>
2017-09-11 00:04:40 +0900

    initial MSD

---
+++ MS_hw01_201302480/app/src/main/java/com/example/jsh/ms_hw01_201302480/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jsh.ms_hw01_201302480;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
