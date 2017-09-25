0c98403140800ca0822299594a34b1112ed1d055
unknown <arcane1028@gmail.com>
2017-09-25 18:22:30 +0900

    lab04 added

---
+++ MS_lab04_2_201302480/app/src/main/java/jsh/example/com/ms_lab04_2_201302480/MenuActivity.java
@@ -0,0 +1,13 @@
+package jsh.example.com.ms_lab04_2_201302480;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
