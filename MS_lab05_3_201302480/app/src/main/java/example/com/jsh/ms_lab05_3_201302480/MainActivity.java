b79ecacb9bba3ac43909818bed847f16ff42d1cb
arcane1028 <arcane1028@gmail.com>
2017-10-09 23:46:13 +0900

    lab5_3 added

---
+++ MS_lab05_3_201302480/app/src/main/java/example/com/jsh/ms_lab05_3_201302480/MainActivity.java
@@ -0,0 +1,13 @@
+package example.com.jsh.ms_lab05_3_201302480;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
