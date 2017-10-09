51b3f40e90baf8b5ec144dbd2f6dfd7418866861
unknown <arcane1028@gmail.com>
2017-10-09 20:26:15 +0900

    lab5 1 2 add

---
+++ MS_lab05_2_201302480/app/src/main/java/example/com/jsh/ms_lab05_2_201302480/SMSReceiver.java
@@ -0,0 +1,15 @@
+package example.com.jsh.ms_lab05_2_201302480;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
