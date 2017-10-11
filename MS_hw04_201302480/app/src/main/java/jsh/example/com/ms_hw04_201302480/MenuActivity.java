dd69ec5e09c3d59efad8c64a6fdbc09a0c7cc624
arcane1028 <arcane1028@gmail.com>
2017-10-12 01:26:32 +0900

    hw04 complete

--- MS_hw04_201302480/app/src/main/java/jsh/example/com/ms_hw04_201302480/MenuActivity.java
+++ MS_hw04_201302480/app/src/main/java/jsh/example/com/ms_hw04_201302480/MenuActivity.java
@@ -15,8 +15,8 @@ import android.widget.Toast;
 public class MenuActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_MENU = 101;
-    Button cameraButton;
     Button locationButton;
+    Button cameraButton;
     Button calendarButton;
     Button goToLoginButton;
 
@@ -26,8 +26,8 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        cameraButton = (Button) findViewById(R.id.cameraButton);
         locationButton = (Button) findViewById(R.id.locationButton);
+        cameraButton = (Button) findViewById(R.id.cameraButton);
         calendarButton = (Button) findViewById(R.id.calendarButton);
         goToLoginButton = (Button) findViewById(R.id.goToLoginButton);
 
@@ -41,18 +41,24 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        cameraButton.setOnClickListener(new View.OnClickListener() {
+        addPermissionEvent(locationButton, Manifest.permission.ACCESS_COARSE_LOCATION, 1);
+        addPermissionEvent(cameraButton, Manifest.permission.CAMERA, 2);
+        addPermissionEvent(calendarButton, Manifest.permission.READ_CALENDAR, 3);
+    }
+
+    private void addPermissionEvent(final Button button, final String permission, final int requestCode) {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int permissionCheck = ContextCompat.checkSelfPermission(MenuActivity.this, Manifest.permission.RECEIVE_SMS);
+                int permissionCheck = ContextCompat.checkSelfPermission(MenuActivity.this, permission);
                 if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-                    //TODO 권한 승인 된 경우
+                    Toast.makeText(getApplicationContext(),  requestCodeToString(requestCode)+" 권한 승인됨", Toast.LENGTH_LONG).show();
                 } else {
                     //아래 if 한번거절하면 true 됨
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MenuActivity.this, Manifest.permission.RECEIVE_SMS)) {
-                        //TODO 권한 거절된 경우
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MenuActivity.this, permission)) {
+                        ActivityCompat.requestPermissions(MenuActivity.this, new String[]{permission}, requestCode);
                     } else {
-                        ActivityCompat.requestPermissions(MenuActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+                        ActivityCompat.requestPermissions(MenuActivity.this, new String[]{permission}, requestCode);
                     }
                 }
             }
@@ -69,35 +75,55 @@ public class MenuActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                //Todo 수정
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
 
-                    intent.putExtra("Permission", "카메라");
-                    intent.putExtra("Check", true);
-
-                    startActivityForResult(intent, REQUEST_CODE_MENU);
-                } else {
-                    //TODO 거절 응답 0
-                }
-                return;
-            }
+        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+        intent.putExtra("Permission", requestCode);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            intent.putExtra("Check", -1);
+        } else {
+            intent.putExtra("Check", 0);
         }
+        startActivityForResult(intent, REQUEST_CODE_MENU);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (requestCode == REQUEST_CODE_MENU) {
-            //Todo 수정
             if (data != null) {
                 Bundle bundle = data.getExtras();
-                String menu = bundle.getString("MENU");
-                Toast.makeText(getApplicationContext(), "메뉴 : " + menu, Toast.LENGTH_LONG).show();
+                int permission = bundle.getInt("Permission");
+                Toast.makeText(getApplicationContext(),  requestCodeToString(permission) +
+                        "권한 "+ resultCodeToString(resultCode)+"됨 resultCode : "+resultCode, Toast.LENGTH_LONG).show();
             }
 
+        }
+    }
+
+    private String resultCodeToString(int resultCode) {
+        switch (resultCode) {
+            case 0:
+                return "거절";
+            case -1:
+                return "승인";
+
+            default:
+                return "에러";
+        }
+    }
+
+    private String requestCodeToString(int requestCode) {
+        switch (requestCode) {
+            case 1:
+                return "위치";
+
+            case 2:
+                return "카몌라";
+
+            case 3:
+                return "달력";
+
+            default:
+                return "디폴트";
         }
     }
 
--- MS_hw04_201302480/app/src/main/java/jsh/example/com/ms_hw04_201302480/ResultActivity.java
+++ MS_hw04_201302480/app/src/main/java/jsh/example/com/ms_hw04_201302480/ResultActivity.java
@@ -1,13 +1,74 @@
 package jsh.example.com.ms_hw04_201302480;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
+
+    Button goToMenuButton;
+    TextView resultText;
+    int check, permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        goToMenuButton = (Button) findViewById(R.id.goToMenuButton);
+        resultText = (TextView) findViewById(R.id.resultText);
+
+        Bundle bundle =  getIntent().getExtras();
+
+
+        permission = bundle.getInt("Permission");
+        check = bundle.getInt("Check");
+        resultText.setText(requestCodeToString(permission) + "권한 "+ resultCodeToString(check)+"됨");
+        Toast.makeText(getApplicationContext(),  requestCodeToString(permission) +
+                "권한 "+ resultCodeToString(check)+"됨", Toast.LENGTH_LONG).show();
+
+        goToMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("Permission", permission);
+                setResult(check, intent);
+                finish();
+            }
+        });
+    }
+
+
+    private String resultCodeToString(int resultCode) {
+        switch (resultCode) {
+            case 0:
+                return "거절";
+
+            case -1:
+                return "승인";
+
+            default:
+                return "에러";
+        }
+    }
+
+    private String requestCodeToString(int requestCode) {
+        switch (requestCode) {
+            case 1:
+                return "위치";
+
+            case 2:
+                return "카몌라";
+
+            case 3:
+                return "달력";
+
+            default:
+                return "디폴트";
+        }
     }
 }
