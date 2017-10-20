298f19d5b19b3d80c96c0068d0e106dcfeee1f8b
unknown <arcane1028@gmail.com>
2017-10-20 16:46:47 +0900

    complete hw06

--- MS_hw06_201302480/app/src/main/java/example/com/jsh/ms_hw06_201302480/MainActivity.java
+++ MS_hw06_201302480/app/src/main/java/example/com/jsh/ms_hw06_201302480/MainActivity.java
@@ -1,51 +1,55 @@
 package example.com.jsh.ms_hw06_201302480;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    ActionBar actionBar;
+;
     WebView webView;
-    EditText editText;
+    EditText inputURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        actionBar = this.getSupportActionBar();
-        actionBar.show();
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {
 
-        webView = (WebView)findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                Toast.makeText(MainActivity.this, "onReceivedError", Toast.LENGTH_LONG).show();
+                super.onReceivedError(view, request, error);
+            }
+        });
 
+        inputURL = (EditText) findViewById(R.id.inputURL);
+        Button goToNaverButton = (Button) findViewById(R.id.goToNaver);
+        Button goToURLButton = (Button) findViewById(R.id.goToURL);
 
-
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.goToNaver:
-                webView.loadUrl("https://www.naver.com/");
-                return true;
-            case R.id.goToURL:
-                webView.loadUrl(editText.getText().toString());
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        goToNaverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String naverUrl = "https://www.naver.com/";
+                inputURL.setText(naverUrl);
+                webView.loadUrl(naverUrl);
+            }
+        });
+        goToURLButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl(inputURL.getText().toString());
+            }
+        });
     }
 }
